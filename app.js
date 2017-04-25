a11c928cc95dbd5c80c29ed583d11da0ab57e6b8
IAmFrench <alexandre@pares.pw>
2017-04-25 15:02:18 +0200

    Réécriture du listener sur les 3 inputs (boutons)

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ class rps {
         this.addPictures() // on ajoute les images
         this.addResult() // on joute le résultat
 
-        switch (this._result) {
-            case 'none': // pas de vainqueurs
-                break;
-            case 'playA':
-                break;
-            case 'playB':
-                break;
-        }
-        console.log(this._result)
+        console.log('Et le gagnant est: ',this._result) // on affiche le vainqueur
     }
 
     get winner () {
         if (this.playA == null) { // on générer aléatoirement un move pour le playerB
             this.setPlayB()
         }
-        
+
         console.log('Joueur A: ', this._playA)
         console.log('Joueur A: ', this._playB)
 
@@ -31,14 +23,15 @@ class rps {
             case 'scissors-paper':
             case 'rock-scissors':
                 this._winner = 'playA'
-                break;
+                break
             case 'rock-paper':
             case 'paper-scissors':
             case 'scissors-rock':
                 this._winner = 'playB'
+                break
             default:
                 this._winner = 'none'
-                break;
+                break
         }
         return this._winner
     }
@@ -56,17 +49,14 @@ class rps {
     }
 
     addPictures() {
-        var imageA
-        var imageB
-
         var images = {
             "rock":"https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png",
             "paper":"https://upload.wikimedia.org/wikipedia/commons/a/af/Rock-paper-scissors_%28paper%29.png",
             "scissors":"https://upload.wikimedia.org/wikipedia/commons/5/5f/Rock-paper-scissors_%28scissors%29.png"
         }
 
-        imageA = `<img src="${images[this._playA]}" alt="${this._playA}">`
-        imageB = `<img src="${images[this._playB]}" alt="${this._playB}">`
+        var imageA = `<img src="${images[this._playA]}" alt="${this._playA}">`
+        var imageB = `<img src="${images[this._playB]}" alt="${this._playB}">`
 
         document.querySelector('.you').innerHTML = imageA
         document.querySelector('.ia').innerHTML = imageB
@@ -90,21 +80,15 @@ class rps {
     }
 }
 
-
-var playA;
+var playA; // mlove du player A
+var game;
 /**
  * On lance la game si on clique sur l'un des boutons
  */
-var game;
-document.querySelector('#rock').addEventListener('click', function (e){
-    playA = e.target.value
+document.querySelectorAll('.butt').forEach(function(button) {
+    button
+    .addEventListener('click', function (e) {
+        playA = e.target.value // on prend la valeur de l'input
     game = new rps(playA)
-})
-document.querySelector('#paper').addEventListener('click', function (e){
-    playA = e.target.value
-    game = new rps(playA)
-})
-document.querySelector('#scissors').addEventListener('click', function (e){
-    playA = e.target.value
-    game = new rps(playA)
-})
+    })
+}, this);
